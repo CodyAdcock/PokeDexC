3283fee6030a6d9e40f454a7f60152e83d2a960f
Cody Adcock <codyadcock10@gmail.com>
2018-09-11 10:21:55 -0600

    Initial Commit

---
+++ PokeDexC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokeDexC
+//
+//  Created by Cody on 9/11/18.
+//  Copyright © 2018 Cody Adcock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokeDexC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PokeDexC
+//
+//  Created by Cody on 9/11/18.
+//  Copyright © 2018 Cody Adcock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
